aac064525fb0e4614d49985a3c04f43b405e0dba
Arslan240 <arslan.jamil1322@gmail.com>
2024-02-07 23:27:17 +0500

    Profile Icon was not showing correct image on Create Post Modal, so removed default url in ProfileIcon.tsx

--- src/components/ProfileIcon/ProfileIcon.tsx
+++ src/components/ProfileIcon/ProfileIcon.tsx
@@ -9,14 +9,14 @@ type Props = {
 }
 
 // name is just provided a letter to remove error of required name
-const ProfileIcon = ({height = 25, imageURL = "https://cms-assets.themuse.com/media/lead/6437.jpg", name = "P"} : Props) => {
+const ProfileIcon = ({height = 25, imageURL, name = "P"} : Props) => {
   // console.log(name)
   return (
     // <div className="profile__icon" style={{height:`${height}px`}}>
     //     <img src={imageURL} alt="" />
     // </div>
     <div className="profile__icon" style={{height:`${height}px`}}>
-        {name !== "" && name !== " " ? 
+        {name !== "" && name !== " " ?
           <Avatar name={name} src={imageURL} round size={height.toString()}/>
           // <Avatar name={name !== "" ? name : "U"} src={imageURL} round size={height.toString()}/>
         : <SpinnerCircularFixed color="var(--linkedin-blue)" size={30}/>}
